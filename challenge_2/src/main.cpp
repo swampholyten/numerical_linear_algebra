f87c0eb52577fedff4b7d659cab7ae9957de2791
= <=>
2025-10-19 01:13:25 +0200

    add challenge 2 starter

---
+++ challenge_2/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Eigen/Eigen>
+#include <iostream>
+#include <lis.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#include "stb/stb_image_write.h"
+
+int main() {
+
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
