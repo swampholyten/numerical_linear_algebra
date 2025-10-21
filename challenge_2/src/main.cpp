90e97855b24ec6298ed1d7e683888b28d1c4dc43
= <=>
2025-10-21 20:16:23 +0200

    add last points

--- challenge_2/src/main.cpp
+++ challenge_2/src/main.cpp
@@ -22,6 +22,10 @@ private:
   Eigen::SparseMatrix<double> Ds; // Degree diagonal matrix for social network
   Eigen::SparseMatrix<double> Ls; // Laplasian for social network
 
+  Eigen::VectorXd fielder_vector;
+  double second_smallest_positive_eigenvalue;
+  int nn, np;
+
 public:
   void create_small_graph() {
     Ag = Eigen::MatrixXd::Zero(9, 9);
@@ -197,6 +201,217 @@ public:
     lis_matrix_destroy(A);
     lis_vector_destroy(x);
   }
+
+  void find_shift_for_acceleration() {
+    Eigen::MatrixXd Ls_dense = Eigen::MatrixXd(Ls);
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(Ls_dense);
+    Eigen::VectorXd eigenvalues = solver.eigenvalues();
+
+    double lamdba_min = eigenvalues.minCoeff();
+    double lamdba_max = eigenvalues.maxCoeff();
+
+    std::cout << "Eigenvalue range: [" << lamdba_min << ", " << lamdba_max
+              << "]" << std::endl;
+
+    // For shifted inverse iteration to find largest eigenvalue efficiently,
+    // we want μ close to lambda_max but not exactly equal
+    double mu = lamdba_max - 0.5;
+
+    LIS_MATRIX A;
+    LIS_VECTOR x;
+    LIS_ESOLVER esolver;
+    LIS_INT n = Ls.rows();
+    LIS_INT iter;
+    LIS_REAL evalue;
+
+    lis_matrix_create(LIS_COMM_WORLD, &A);
+    lis_matrix_set_size(A, n, 0);
+
+    for (int k = 0; k < Ls.outerSize(); ++k) {
+      for (Eigen::SparseMatrix<double>::InnerIterator it(Ls, k); it; ++it) {
+        lis_matrix_set_value(LIS_INS_VALUE, it.row(), it.col(), it.value(), A);
+      }
+    }
+    lis_matrix_assemble(A);
+
+    lis_vector_create(LIS_COMM_WORLD, &x);
+    lis_vector_set_size(x, n, 0);
+    lis_vector_set_all(1.0, x);
+
+    lis_esolver_create(&esolver);
+
+    char options[256];
+    sprintf(options, "-e rqi -eshift %f -etol 1e-8 -emaxiter 10000", mu);
+    lis_esolver_set_option(options, esolver);
+
+    lis_esolve(A, x, &evalue, esolver);
+    lis_esolver_get_iter(esolver, &iter);
+
+    std::cout << "Shift mu = " << mu << std::endl;
+    std::cout << "Eigenvalue with shift: " << evalue << std::endl;
+    std::cout << "Iterations with shift: " << iter << std::endl;
+
+    lis_esolver_destroy(esolver);
+    lis_matrix_destroy(A);
+    lis_vector_destroy(x);
+  }
+
+  void find_second_smallest_positive_eigenvalue() {
+    LIS_MATRIX A;
+    LIS_VECTOR x;
+    LIS_ESOLVER esolver;
+    LIS_INT n = Ls.rows();
+    LIS_INT iter;
+    LIS_REAL evalue;
+
+    lis_matrix_create(LIS_COMM_WORLD, &A);
+    lis_matrix_set_size(A, n, 0);
+
+    for (int k = 0; k < Ls.outerSize(); ++k) {
+      for (Eigen::SparseMatrix<double>::InnerIterator it(Ls, k); it; ++it) {
+        lis_matrix_set_value(LIS_INS_VALUE, it.row(), it.col(), it.value(), A);
+      }
+    }
+    lis_matrix_assemble(A);
+
+    lis_vector_create(LIS_COMM_WORLD, &x);
+    lis_vector_set_size(x, n, 0);
+    lis_vector_set_all(1.0, x);
+
+    lis_esolver_create(&esolver);
+
+    lis_esolver_set_option((char *)"-e ii -etol 1e-10 -emaxiter 10000",
+                           esolver);
+    lis_esolve(A, x, &evalue, esolver);
+
+    double smallest = evalue;
+    std::cout << "Smallest eigenvalue: " << smallest << std::endl;
+
+    double shift = smallest + 0.01;
+
+    lis_esolver_destroy(esolver);
+    lis_esolver_create(&esolver);
+
+    char options[256];
+    sprintf(options, "-e ii -eshift %f -etol 1e-10 -emaxiter 10000", shift);
+
+    lis_esolver_set_option(options, esolver);
+
+    lis_vector_set_all(1.0, x);
+
+    lis_esolve(A, x, &evalue, esolver);
+    lis_esolver_get_iter(esolver, &iter);
+
+    second_smallest_positive_eigenvalue = evalue;
+
+    fielder_vector = Eigen::VectorXd::Zero(n);
+
+    for (int i = 0; i < n; i++) {
+      LIS_SCALAR val;
+      lis_vector_get_value(x, i, &val);
+      fielder_vector(i) = val;
+    }
+
+    std::cout << "Second smallest positive eigenvalue: "
+              << second_smallest_positive_eigenvalue << std::endl;
+    std::cout << "Iteration count: " << iter << std::endl;
+
+    lis_esolver_destroy(esolver);
+    lis_matrix_destroy(A);
+    lis_vector_destroy(x);
+  }
+
+  void find_second_smallest_positive_eigenvalue_with_eigen() {
+
+    Eigen::MatrixXd Ls_dense = Eigen::MatrixXd(Ls);
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(Ls_dense);
+    Eigen::VectorXd eigenvalues = solver.eigenvalues();
+    Eigen::MatrixXd eigenvectors = solver.eigenvectors();
+
+    std::vector<std::pair<double, int>> positiveEigs;
+    for (int i = 0; i < eigenvalues.size(); i++) {
+      if (eigenvalues(i) > 1e-10) {
+        positiveEigs.push_back({eigenvalues(i), i});
+      }
+    }
+
+    std::sort(positiveEigs.begin(), positiveEigs.end());
+
+    if (positiveEigs.size() >= 2) {
+
+      second_smallest_positive_eigenvalue = positiveEigs[1].first;
+      fielder_vector = eigenvectors.col(positiveEigs[1].second);
+
+      std::cout << "Second smallest positive eigenvalue (Eigen): "
+                << second_smallest_positive_eigenvalue << std::endl;
+    }
+  }
+
+  void sort_fielder_vector() {
+    np = 0;
+    nn = 0;
+
+    for (int i = 0; i < fielder_vector.size(); i++) {
+      if (fielder_vector(i) > 0) {
+        np++;
+      } else {
+        nn++;
+      }
+    }
+
+    std::cout << "Number of positive entries (np): " << np << std::endl;
+    std::cout << "Number of negative entries (nn): " << nn << std::endl;
+  }
+
+  void reorder_adjacency_matrix() {
+    int n = As.rows();
+
+    std::vector<int> perm_indices;
+
+    for (int i = 0; i < n; i++) {
+      if (fielder_vector(i) > 0) {
+        perm_indices.push_back(i);
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (fielder_vector(i) <= 0) {
+        perm_indices.push_back(i);
+      }
+    }
+
+    Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> P(n);
+
+    for (int i = 0; i < n; i++) {
+      P.indices()(perm_indices[i]) = i;
+    }
+
+    Eigen::SparseMatrix<double> Aord = P * As * P.transpose();
+
+    int nonzeros_offdiag_reordered = 0;
+
+    for (int i = 0; i < np; i++) {
+      for (int j = np; j < n; j++) {
+        if (Aord.coeff(i, j) != 0) {
+          nonzeros_offdiag_reordered++;
+        }
+      }
+    }
+
+    int nonzeros_offdiag_original = 0;
+    for (int i = 0; i < np; i++) {
+      for (int j = np; j < n; j++) {
+        if (As.coeff(i, j) != 0) {
+          nonzeros_offdiag_original++;
+        }
+      }
+    }
+
+    std::cout << "Nonzeros in Aord(1:" << np << ", " << (np + 1) << ":" << n
+              << "): " << nonzeros_offdiag_reordered << std::endl;
+    std::cout << "Nonzeros in As(1:" << np << ", " << (np + 1) << ":" << n
+              << "): " << nonzeros_offdiag_original << std::endl;
+  }
 };
 
 int main(int argc, char *argv[]) {
@@ -224,6 +439,22 @@ int main(int argc, char *argv[]) {
 
   // Point 7
   c2.find_largest_eigenvalues();
+
+  // FIX(?)
+  // Point 8
+  c2.find_shift_for_acceleration();
+
+  // FIX
+  // Point 9
+  // c2.find_second_smallest_positive_eigenvalue();
+  c2.find_second_smallest_positive_eigenvalue_with_eigen();
+
+  // Point 10
+  c2.sort_fielder_vector();
+
+  // FIX(?)
+  // Point 11
+	c2.reorder_adjacency_matrix();
 
   lis_finalize();
 
